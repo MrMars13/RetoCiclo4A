6a71ff9fc25d8e4d6589522179cb452fa9b3defc
Andres Argotty <andresmauricioarg@gmail.com>
2021-11-13 13:10:35 -0500

    Versión 1.0.0 de EmpresaApp

---
+++ BackendAdmin/src/models/empleado.model.ts
@@ -0,0 +1,86 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Empleado extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombres: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Apellidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Telefono: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Direccion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Edad: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  FechaNacimiento: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Sueldo: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  EsDirectivo: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  EsCliente: number;
+
+  @property({
+    type: 'string',
+  })
+  empresaId?: string;
+
+  constructor(data?: Partial<Empleado>) {
+    super(data);
+  }
+}
+
+export interface EmpleadoRelations {
+  // describe navigational properties here
+}
+
+export type EmpleadoWithRelations = Empleado & EmpleadoRelations;
---
+++ BackendAdmin/src/services/notificacion.service.ts
@@ -0,0 +1,31 @@
+import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class NotificacionService {
+  constructor(/* Add @inject to inject parameters */) {}
+
+  /*
+   * Servicio de mensajeria por Twilio
+   */
+
+  NotificacionesSMS():void{
+    console.log("hola");
+    const accountSid = 'ACab27ac8cc28762df0da579e0dbfce6ac'; // Your Account SID from www.twilio.com/console
+const authToken = '2518ac2a34f631bf034354fba26f1f5a'; // Your Auth Token from www.twilio.com/console
+
+const twilio = require('twilio');
+const client = new twilio(accountSid, authToken);
+
+client.messages
+  .create({
+    body: 'Prueba de mensajería',
+    to: '+57 3023557964', // Text this number
+    from: '+18123625389', // From a valid Twilio number
+  })
+  .then((message: any) => console.log(message.sid));
+
+  }
+}
+
+// NOTA PARA PAOLA:
+// Para que puedas probar la función de mensajería debes cambiar las constantes de accountSID y de authtoken por los valores que están en tu twilio, además cambiar el numero de from, y el de to debe ser tu numero registrado en twilio. Para terminar debes correr la app y desde el API explorer entras al EmpleadoController y creas uno nuevo desde el método Post, en el momento que lo creas, debe llegarte el msj.  :D
